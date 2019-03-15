450c1a173fa9364ec97149e8518edf8e6b6494a1
Jelena Kocic <kocicjelena@gmail.com>
2019-03-15 14:27:44 +0100

    Update wsgy.py

--- wsgy.py
+++ wsgy.py
@@ -8,16 +8,16 @@ from app import app
 app.run(debug = True)
 #app = Flask(__name__)
 @rpc('hello')
-def hellorpc(arg1, arg2, arg3):
-	return "%s-%s-%s" (arg3, arg2, arg1)
-  def hello():
+def hellorpc(foo, bar):
+	return "%s-%s-%s" (foo, bar)
+def hello():
 	return "Hello World"
-	uwsgi.register_rpc("hello", hello)
+uwsgi.register_rpc("hello", hello)
 def helloapp(env, start_response):
 	start_response('200 Ok', [('Content-Type', 'text/html')])
-	return uwsgi.rpc('10.0.0.2:3031', 'hello')
+	return uwsgi.rpc('127.0.0.1:3031', 'hello')
 def hello_file(num):
 	print "/tmp has been modified !!!"
 uwsgi.register_signal(17, "worker", hello_file)
 uwsgi.add_file_monitor(17, "/tmp")
-uwsgi.applications = {'': application, '/app': app,}
+uwsgi.applications = {'': application, '/app': app, '/helloapp': helloapp}
